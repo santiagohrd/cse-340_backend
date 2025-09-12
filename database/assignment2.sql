c92be95d71cc69089220961d361a6095804e9f80
SantiagoHrd <santiagohrd1@gmail.com>
2025-09-12 17:38:49 -0500

    db queries

---
+++ database/assignment2.sql
@@ -0,0 +1,38 @@
+-- Tony Stark insert
+INSERT INTO public.account
+	(
+		account_firstname,
+		account_lastname,
+		account_email,
+		account_password
+	)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- Tony Stark update
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+-- Delete Tony Stark 
+DELETE FROM public.account
+WHERE account_email = 'tony@starkent.com';
+
+  -- The description update
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+
+-- Select query using a JOIN
+SELECT inv_make, inv_model, classification_name
+FROM public.inventory AS i
+INNER JOIN public.classification AS c
+	ON i.classification_id = c.classification_id
+WHERE classification_name = 'Sport';
+
+-- The inv_image and inv_thumbnail update query 
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+	inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+
