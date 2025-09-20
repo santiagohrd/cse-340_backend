bc299872b109f6968b4965c4c4a6aecff0e17f5d
SantiagoHrd <santiagohrd1@gmail.com>
2025-09-20 04:33:23 -0500

    w3 task Add the intentional error

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -6,4 +6,10 @@ baseController.buildHome = async function(req, res){
   res.render("index", {title: "Home", nav})
 }
 
+baseController.throwError = async function(req, res, next){
+    const error = new Error("This is an intentional error for testing.");
+    error.status = 500;
+    next(error);
+}
+
 module.exports = baseController
